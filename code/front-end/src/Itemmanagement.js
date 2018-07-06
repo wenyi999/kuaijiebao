e810a8f21371915169ee804c8d09e638f3a9840f
samchen <a454545@sjtu.edu.cn>
2018-07-06 17:00:03 +0800

    理财产品管理界面初稿

--- code/front-end/src/Itemmanagement.js
+++ code/front-end/src/Itemmanagement.js
@@ -1,14 +1,58 @@
-import {Affix, Row, Col, Divider} from 'antd';
-import React, {Component} from 'react';
-import {Link} from "react-keeper";
+import React, { Component } from 'react';
 
-class Itemmanagement extends Component {
-    render() {
+import './App.css';
 
-        return (
-            <div> Itemmanagement </div>
-        )
-    }
+import { Table} from 'antd';
+
+const { Column } = Table;
+
+const debts_list = []
+for (let i = 0; i <25; i++) {
+  debts_list.push({
+    key : i+1,
+    product_name: '债券No.'+ (i+1),
+    money:45+i,
+    rate: "1%",
+    date:"2018-07-09 ~ 2018-09-09"
+  })
 }
 
-export default  Itemmanagement ;
+class Itemmanagement extends Component {
+  render(){
+    return (
+      <Table dataSource={debts_list}>
+        <Column
+          title="购买单号"
+          dataIndex="key"
+          key="key"
+        />
+        <Column
+          title="产品名称"
+          dataIndex="product_name"
+          key="product_name"
+        />
+        <Column
+          title="投资金额"
+          dataIndex="money"
+          key="money"
+        />
+        <Column
+          title="利率"
+          dataIndex="rate"
+          key="rate"
+        />
+        <Column
+          title="期限"
+          dataIndex="date"
+          key="date"
+        />
+        <Column
+          title="兑现"
+          key="action"
+        />
+      </Table>
+    )
+  }
+}
+
+export default Itemmanagement;
